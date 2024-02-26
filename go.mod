8771dd0698aad38bea2aac6a8c4386e1e324ebd8
Saurlax <saurlax@qq.com>
2024-02-26 21:59:00 +0800

    feat: basic type

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.21.6
 
 require (
 	github.com/cakturk/go-netstat v0.0.0-20200220111822-e5b49efee7a5 // indirect
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	github.com/mitchellh/go-ps v1.0.0 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,31 +2,62 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"os"
 	"time"
 
 	"github.com/cakturk/go-netstat/netstat"
 	"github.com/mitchellh/go-ps"
+	"github.com/syndtr/goleveldb/leveldb"
+	"gopkg.in/yaml.v3"
 )
 
 var (
-	packets = make(chan NetStatData)
+	config  Config = Config{}
+	packets        = make(chan NetStatData)
+	db      *leveldb.DB
 )
 
 type NetStatData struct {
 	LocalAddr  net.IP
+	LocalPort  uint16
 	RemoteAddr net.IP
+	RemotePort uint16
 	Executable string
 }
 
-func capture(c chan<- NetStatData) {
+type Config struct {
+	CaptureInterval int
+	CheckInterval   int
+}
+
+type IPRecord struct {
+	IP   net.IP
+	Risk int
+	// 0 safe
+	// 1 unknown / low
+	// 1 suspicious
+	// 2 malicious
+	Description string
+	// ASN    string
+	// TODO: geoip
+	ConfirmedBy string
+}
+
+// Threat Intelligence Center
+type TIX interface {
+	CheckIPs(ips []net.IP) []IPRecord
+}
+
+func capture() {
 	time.Sleep(1 * time.Second)
 	for {
 		tabs, err := netstat.TCPSocks(func(s *netstat.SockTabEntry) bool {
 			return s.State == netstat.Established
 		})
 		if err != nil {
-			fmt.Println("Error:", err)
+			log.Println("Cannot get TCPSocks", err)
 			continue
 		}
 
@@ -36,9 +67,11 @@ func capture(c chan<- NetStatData) {
 				fmt.Println("Error:", err)
 				continue
 			}
-			c <- NetStatData{
+			packets <- NetStatData{
 				LocalAddr:  e.LocalAddr.IP,
+				LocalPort:  e.LocalAddr.Port,
 				RemoteAddr: e.RemoteAddr.IP,
+				RemotePort: e.RemoteAddr.Port,
 				Executable: proc.Executable(),
 			}
 			// FIXME: not absolute path
@@ -46,9 +79,36 @@ func capture(c chan<- NetStatData) {
 	}
 }
 
-func main() {
-	go capture(packets)
-	for data := range packets {
-		fmt.Printf(data.LocalAddr.String() + " -> " + data.RemoteAddr.String() + " : " + data.Executable + "\n")
+func check() {
+	for {
+		time.Sleep(1 * time.Second)
+		c := <-packets
+		println(c.RemoteAddr.String())
 	}
+}
+
+func init() {
+	// config
+	data, err := os.ReadFile("config.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// db
+	db, err = leveldb.OpenFile("db", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+}
+
+func main() {
+	log.Println("Service started")
+	println(config.CaptureInterval, config.CheckInterval)
+	go capture()
+	check()
 }
