a527c3008b2e847155d3c5eaa6857d00ae1003e1
Saurlax <saurlax@qq.com>
2024-04-20 14:23:12 +0800

    feat: remove duplicates

--- netvigil/netstat.go
+++ netvigil/netstat.go
@@ -1,6 +1,7 @@
 package netvigil
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/cakturk/go-netstat/netstat"
@@ -10,19 +11,30 @@ import (
 
 var NetstatCh = make(chan netstat.SockTabEntry, 1000)
 
+var cache = make(map[string]time.Time)
 var filter = func(s *netstat.SockTabEntry) bool {
 	return s.State == netstat.Established && !s.RemoteAddr.IP.IsLoopback()
 }
 
 func capture() {
+	for e, t := range cache {
+		if time.Since(t) > 60*time.Second {
+			delete(cache, e)
+		}
+	}
+
 	tcps, _ := netstat.TCPSocks(filter)
 	tcp6s, _ := netstat.TCP6Socks(filter)
 	udps, _ := netstat.UDPSocks(filter)
 	udp6s, _ := netstat.UDP6Socks(filter)
-
 	entries := append(append(append(tcps, tcp6s...), udps...), udp6s...)
 
 	for _, e := range entries {
+		key := fmt.Sprintf("%s-%s", e.LocalAddr.String(), e.RemoteAddr.String())
+		if _, ok := cache[key]; ok {
+			continue
+		}
+		cache[key] = time.Now()
 		proc, err := ps.FindProcess(e.Process.Pid)
 		if err == nil && proc != nil {
 			path, err := proc.Path()
