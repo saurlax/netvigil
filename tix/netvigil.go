1baadd433cfebfd64b583836cf44dba3801bb5dc
sikelifei <2463704912@qq.com>
2024-05-30 17:19:47 +0800

    Changes to be committed: 	modified:   tix/index.go 	new file:   tix/netvigil.go

---
+++ tix/netvigil.go
@@ -0,0 +1,63 @@
+package tix
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/cakturk/go-netstat/netstat"
+	"github.com/saurlax/netvigil/netvigil"
+)
+
+type Netvigil struct {
+	Server string
+	Token  string
+}
+
+type NetvigilRequest struct {
+	Token string   `json:"token"`
+	IPs   []string `json:"ips"`
+}
+
+type NetvigilResponse struct {
+	Records []netvigil.Record `json:"records"`
+}
+
+func (t *Netvigil) Check(netstats []netstat.SockTabEntry) []netvigil.Record {
+	var records []netvigil.Record
+	var ips []string
+	for _, v := range netstats {
+		if !v.RemoteAddr.IP.IsPrivate() {
+			ips = append(ips, v.RemoteAddr.IP.String())
+		}
+	}
+	if len(ips) == 0 {
+		return records
+	}
+
+	requestBody, err := json.Marshal(NetvigilRequest{
+		Token: t.Token,
+		IPs:   ips,
+	})
+	if err != nil {
+		fmt.Println("[Netvigil] Failed to marshal request:", err)
+		return records
+	}
+
+	res, err := http.Post(fmt.Sprintf("%s/api/check", t.Server), "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		fmt.Println("[Netvigil] Failed to request:", err)
+		return records
+	}
+	defer res.Body.Close()
+
+	var response NetvigilResponse
+	err = json.NewDecoder(res.Body).Decode(&response)
+	if err != nil {
+		fmt.Println("[Netvigil] Failed to decode response:", err)
+		return records
+	}
+
+	return response.Records
+}
