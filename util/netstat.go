61a9529a28a4601508a54b35d3ad4799638916ee
Saurlax <saurlax@qq.com>
2024-07-06 13:24:31 +0800

    chore: tidy up and docs

--- util/netstat.go
+++ util/netstat.go
@@ -5,7 +5,6 @@ import (
 	"net"
 	"time"
 
-	"github.com/IncSW/geoip2"
 	"github.com/cakturk/go-netstat/netstat"
 	"github.com/keybase/go-ps"
 	"github.com/spf13/viper"
@@ -64,6 +63,28 @@ func Capture() {
 			RemoteIP:   e.RemoteAddr.IP.String(),
 			RemotePort: e.RemoteAddr.Port,
 		}
+
+		// get the location of the IP address
+		ip := net.ParseIP(n.RemoteIP)
+		record, err := GeoLiteCity.Lookup(ip)
+		if err == nil {
+			countryName := record.Country.Names["zh-CN"]
+			if countryName == "" {
+				countryName = record.Country.Names["en"]
+			}
+			cityName := record.City.Names["zh-CN"]
+			if cityName == "" {
+				cityName = record.City.Names["en"]
+			}
+			n.Location = countryName
+			if cityName != "" {
+				if n.Location != "" {
+					n.Location += " "
+				}
+				n.Location += cityName
+			}
+		}
+
 		// get the executable path of the process
 		if e.Process != nil {
 			proc, err := ps.FindProcess(e.Process.Pid)
@@ -73,26 +94,6 @@ func Capture() {
 					n.Executable = path
 				}
 			}
-		}
-		// get the location of the IP address
-
-		reader, err := geoip2.NewCityReaderFromFile("./GeoLite2-City.mmdb")
-		if err != nil {
-			panic(err)
-		}
-		ip := net.ParseIP(n.RemoteIP)
-		record, err := reader.Lookup(ip)
-		if err != nil {
-			n.Location = "Unknown"
-		} else {
-			location := ""
-			if record.Country.Names["zh-CN"] != "" {
-				location += record.Country.Names["zh-CN"]
-			}
-			if record.City.Names["zh-CN"] != "" {
-				location += " " + record.City.Names["zh-CN"]
-			}
-			n.Location = location
 		}
 
 		n.Save()
