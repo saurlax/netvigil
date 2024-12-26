ff0bfa155e3275e57d28daf1cfab6ce4775e8626
LuffyS93 <1725237709@qq.com>
2024-12-26 17:47:12 +0800

    fix: Modify a variable according to previous commit

--- util/config.go
+++ util/config.go
@@ -8,7 +8,7 @@ import (
 
 func init() {
 	viper.SetDefault("capture_interval", "10s")
-	viper.SetDefault("check_interval", "60s")
+	viper.SetDefault("check_period", "60s")
 	viper.SetDefault("buffer_size", 2000)
 	viper.SetDefault("page_size", 200)
 	viper.SetConfigFile("config.toml")
