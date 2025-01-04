6a5d0a3279de664bae0c9e9fee41de61f26a7fbe
LuffyS93 <1725237709@qq.com>
2025-01-04 12:24:50 +0800

    modified some screen display func

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -39,7 +39,66 @@ const mainOption = computed(() => {
   }
 })
 
-const pieViewOption = computed(() => {
+const pieViewOption1 = computed(() => {
+  return {
+    dataset: {
+      source: [
+        ['威胁等级', '数量'],
+        ['安全', stats.value[8][1]],  // 近七日安全的数量
+        ['正常', stats.value[8][2]],  // 近七日正常的数量
+        ['可疑', stats.value[8][3]],  // 近七日可疑的数量
+        ['恶意', stats.value[8][4]],  // 近七日恶意的数量
+        ['未知', stats.value[8][5]],  // 近七日未知的数量
+      ],
+    },
+    color: ['#5470c6', '#91cc75', '#73c0de', '#fac858', '#ee6666'],
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b}: {c} ({d}%)',
+    },
+    series: [
+      {
+        type: 'pie',
+        radius: '55%',
+        center: ['50%', '50%'],
+        data: [
+          { value: stats.value[8][1], name: '安全' },
+          { value: stats.value[8][2], name: '正常' },
+          { value: stats.value[8][3], name: '可疑' },
+          { value: stats.value[8][4], name: '恶意' },
+          { value: stats.value[8][5], name: '未知' },
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowOffsetY: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+          },
+        },
+      },
+    ],
+  }
+})
+
+const pieViewOption2 = computed(() => {
+  return {
+    dataset: {
+      source: stats.value,
+    }, backgroundColor: '',
+    color: ['#5470c6', '#91cc75', '#73c0de', '#fac858', '#ee6666'],
+    tooltip: {},
+    series: [
+      {
+        type: 'pie',
+        seriesLayoutBy: 'row',
+        encode: { itemName: 0, value: 8 },
+      },
+    ],
+  }
+})
+
+const pieViewOption3 = computed(() => {
   return {
     dataset: {
       source: stats.value,
@@ -125,7 +184,7 @@ const lineTrendOption = computed(() => {
     <div class="subviews">
       <div class="subview-chart">
         <h3>近七日威胁度</h3>
-        <VChart :option="pieViewOption" theme="dark" autoresize />
+        <VChart :option="pieViewOption1" theme="dark" autoresize />
       </div>
       <div class="subview-chart">
         <h3>可疑及以上威胁度的频率</h3>
@@ -141,11 +200,11 @@ const lineTrendOption = computed(() => {
       </div>
       <div class="subview-chart">
         <h3>情报来源占比</h3>
-        <VChart :option="pieViewOption" theme="dark" autoresize /> 
+        <VChart :option="pieViewOption2" theme="dark" autoresize /> 
       </div>
       <div class="subview-chart">
         <h3>To be continue</h3>
-        <VChart :option="pieViewOption" theme="dark" autoresize />
+        <VChart :option="pieViewOption3" theme="dark" autoresize />
       </div>
     </div>
   </div>
