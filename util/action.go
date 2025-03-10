b06763f39999624e6f79b6e51d2b3290991ff660
LuffyS93 <1725237709@qq.com>
2025-03-10 18:14:07 +0800

    针对windows编码问题判断临时添加了一个功能

--- util/action.go
+++ util/action.go
@@ -1,7 +1,9 @@
 package util
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -9,7 +11,10 @@ import (
 	"strings"
 	"time"
 
+	"github.com/endeveit/guesslanguage"
 	"github.com/gen2brain/beeep"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
 )
 
 // 获取操作系统类型
@@ -17,15 +22,36 @@ func getOS() string {
 	return runtime.GOOS
 }
 
+func decodeOutput(output []byte) string {
+	reader := transform.NewReader(bytes.NewReader(output), simplifiedchinese.GBK.NewDecoder())
+	decoded, err := io.ReadAll(reader)
+	if err != nil {
+		return string(output)
+	}
+	return string(decoded)
+}
+
 // / Windows: 检查防火墙规则是否已存在
 func firewallRuleExistsWindows(ip string, direction string) bool {
 	checkCmd := exec.Command("netsh", "advfirewall", "firewall", "show", "rule", "name=netvigil_block_"+direction+"_"+ip)
 	output, err := checkCmd.Output()
+	// log.Printf("output: %v", decodeOutput(output))
 	if err != nil {
-		log.Printf("Error checking Windows firewall rule: %v\n", err)
+		lang, langErr := guesslanguage.Guess(string(output))
+		if langErr != nil {
+			return false
+		}
+		switch lang {
+		case "zh":
+			log.Printf("Error checking Windows firewall rule: %v\n", decodeOutput(output))
+		case "en":
+			log.Printf("Error checking Windows firewall rule: %v\n", string(output))
+		default:
+			log.Printf("Error checking Windows firewall rule. Maybe we do not support the language you're using")
+		}
 		return false
 	}
-	return strings.Contains(string(output), "Rule Name: netvigil_block_"+direction+"_"+ip)
+	return strings.Contains(string(output), "netvigil_block_"+direction+"_"+ip)
 }
 
 // Linux: 检查 iptables 规则是否已存在
@@ -48,7 +74,7 @@ func AddFireWallRule(ip string) error {
 			in.Stdout = os.Stdout
 			in.Stderr = os.Stderr
 			if err := in.Run(); err != nil {
-				log.Printf("Failed to add inbound firewall rule for %s: %v\n", ip, err)
+				log.Printf("Failed to add inbound firewall rule for %s: %v\nMaybe you need to check if you're root", ip, err)
 				return err
 			} else {
 				log.Printf("Inbound firewall rule added for %s\n", ip)
@@ -63,7 +89,7 @@ func AddFireWallRule(ip string) error {
 			out.Stdout = os.Stdout
 			out.Stderr = os.Stderr
 			if err := out.Run(); err != nil {
-				log.Printf("Failed to add outbound firewall rule for %s: %v\n", ip, err)
+				log.Printf("Failed to add outbound firewall rule for %s: %v\nMaybe you need to check if you're root", ip, err)
 				return err
 			} else {
 				log.Printf("Outbound firewall rule added for %s\n", ip)
@@ -75,7 +101,7 @@ func AddFireWallRule(ip string) error {
 		if !firewallRuleExistsLinux(ip, "in") {
 			in := exec.Command("iptables", "-A", "INPUT", "-s", ip, "-j", "DROP")
 			if err := in.Run(); err != nil {
-				log.Printf("Failed to add inbound iptables rule for %s: %v\n", ip, err)
+				log.Printf("Failed to add inbound iptables rule for %s: %v\nMaybe you need to check if you're root", ip, err)
 				return err
 			} else {
 				log.Printf("Inbound iptables rule added for %s\n", ip)
@@ -87,7 +113,7 @@ func AddFireWallRule(ip string) error {
 		if !firewallRuleExistsLinux(ip, "out") {
 			out := exec.Command("iptables", "-A", "OUTPUT", "-d", ip, "-j", "DROP")
 			if err := out.Run(); err != nil {
-				log.Printf("Failed to add outbound iptables rule for %s: %v\n", ip, err)
+				log.Printf("Failed to add outbound iptables rule for %s: %v\nMaybe you need to check if you're root", ip, err)
 				return err
 			} else {
 				log.Printf("Outbound iptables rule added for %s\n", ip)
