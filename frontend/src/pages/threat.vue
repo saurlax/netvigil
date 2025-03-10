3939d5568aa01711ac1c011afc9e4c6069e30403
LuffyS93 <1725237709@qq.com>
2025-03-10 18:09:41 +0800

    对threat界面添加了手动加入威胁ip的方法

--- frontend/src/pages/threat.vue
+++ frontend/src/pages/threat.vue
@@ -13,6 +13,8 @@ const credibilityLevel = ["低", "中", "高"];
 const threats = ref<Threat[]>([])
 const router = useRouter()
 
+const setIP = ref('')
+
 onMounted(() => {
   axios.get('/api/threats', {
     headers: { Authorization: `Bearer ${user.value?.token}` }
@@ -26,6 +28,40 @@ onMounted(() => {
     }
   })
 })
+
+const isValidIP = (ip: string): boolean => {
+  const ipv4Regex = /^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}$/;
+  const ipv6Regex = /^([0-9a-fA-F]{1,4}:){7}([0-9a-fA-F]{1,4}|:)$/;
+  return ipv4Regex.test(ip) || ipv6Regex.test(ip);
+};
+
+const addFireWallRule = async (ip: string) => {
+  if (!ip.trim()) {
+    ElMessage.error('IP不能为空')
+    return
+  }
+  if (!isValidIP(ip)) {
+    ElMessage.error('请输入正确的ip！')
+    return
+  }
+  ElMessageBox.confirm('确定添加该威胁记录吗?', '提示', {
+    type: 'warning'
+  }).then(() => {
+    axios.post(`/api/threats`, { ip } ,{
+      headers: { Authorization: `Bearer ${user.value?.token}` }
+    }).then(() => {
+      ElMessage.success('添加成功')
+      setIP.value = ''
+      router.go(0)
+    }).catch(e => {
+      if (e.response.status === 401) {
+        
+      } else {
+        ElMessage.error(e.response.data.error ?? e.message)
+      }
+    })
+  })
+}
 
 const deleteFireWallRule = async (ip: string) => {
   ElMessageBox.confirm('确定删除该威胁记录吗?', '提示', {
@@ -102,9 +138,20 @@ const data = computed(() => {
 </script>
 
 <template>
+  <div style="margin-top: 20px">
+    <ElInput v-model="setIP" placeholder="输入IP" style="width: 200px; margin-right: 10px;" />
+    <ElButton type="primary" @click="addFireWallRule(setIP)">添加威胁ip</ElButton>
+  </div>
   <ElAutoResizer>
     <template #default="{ height, width }">
       <ElTableV2 :columns="columns" :data="data || []" :height="height" :width="width" />
     </template>
   </ElAutoResizer>
 </template>
+
+<!-- <ElSelect v-model="setRiskLevel" placeholder="选择威胁度" style="width: 150px; margin-right: 10px;">
+      <ElOption v-for="(label, index) in riskLevel" :key="index" :label="label" :value="index" />
+    </ElSelect>
+    <ElSelect v-model="setCredibilityLevel" placeholder="选择可信度" style="width: 150px; margin-right: 10px;">
+      <ElOption v-for="(label, index) in credibilityLevel" :key="index" :label="label" :value="index" />
+    </ElSelect> -->
